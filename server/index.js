a35609be4b26142463b48d4c7371a5cd931d01b2
llcain <llcain2012@gmail.com>
2024-02-06 10:34:11 -0600

    Added middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
 require('dotenv').config();
 
 const express = require('express');
-
-const app = express();
-
-
-
 const mongoose = require('mongoose');
 
-mongoose.connect(mongo_URI);
+// express app
+const app = express();
 
-app.listen(3001, () => {
-    console.log("Server runs perfectly!");
-}) 
+// middleware
+app.use(express.json())
+
+app.use((req,res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
+// connect to db
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listening for request
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port", process.env.PORT);
+        }) 
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
