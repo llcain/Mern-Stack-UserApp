2b127697f179d17c2e50728857c981a3f36941fd
llcain <llcain2012@gmail.com>
2024-02-08 13:31:07 -0600

    Updated index.js file

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.get("/getUsers", (req, res) => {
 });
 
 app.post("/createUser", async (req, res) => {
-    const user = req.body
+    const user = req.body;
     const newUser = new UserModel(user);
     await newUser.save();
     
